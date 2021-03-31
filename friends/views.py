e737794bff1bc2cffe52c910b47b5faf3063cfe0
Austin Grashoff <>
2021-03-31 02:06:34 -0500

    updating friend templates/views

--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+@login_required
+def send_friend_request(request, userID)
+    sender = request.user
+    receiver = user.objects.get(id=userID)
+    friend_request, created = FriendRequest.objects.get_or_create(se der=sender, receiver=receiver)
+    if created:
+        return HttpResponse('friend request sent')
+    else:
+        return HttpResponse('friend request was already sent')
