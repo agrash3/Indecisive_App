02c0dacc3e2c3d72afed0fcb730d00072a973569
Austin Grashoff <>
2021-04-28 00:57:21 -0500

    added yelp api

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,11 +7,14 @@ from django.template import loader
 from django.template.loader import get_template
 from polls.models import Food_Place_ID_Yelp,Food_Place_ID_Zomato, Recipe, User_Detail
 from django.contrib.auth.models import User
+from yelpapi import YelpAPI
 
+import httplib2
 import json
 import urllib
 import requests
 
+yelp_api = YelpAPI("-MCFWTR_20SoAipQQk9m-g13PQeY1fsjteybarMM2hhNRZn01jb_9r_Z3nwABH63toHQ_qvPWb_8l6qYoriGdn8dIilN8-If8C6pSRdaejkFKkAXba1nlLHure6IYHYx")
 #the following setups the search page with a simple search box to get user input
 ## template contains an input forum for the user to type in the Restaurant Name
 def find_restaurants(request):
@@ -75,51 +78,20 @@ def restaurants_results(request):
 		#food place contains the restaurants Id which is used in the Yelp API call
 		food_place_name=search_results1
 		headers = 	{
-					'authorization': "Bearer 1_-tP4IlMNVpRBOj68A5aZJ4FwHdMGCps6xN9PFV0q1AmreUfNclD1Hw0bqQuCSfjthDFl4JQGtfTmvI321ffJ6LcPZ0O2XDYfa5OedFipN4Riw7iibTBCvUR6fVWnYx",
-					'cache-control': "no-cache",
+                    'authorization': "Bearer 1_-tP4IlMNVpRBOj68A5aZJ4FwHdMGCps6xN9PFV0q1AmreUfNclD1Hw0bqQuCSfjthDFl4JQGtfTmvI321ffJ6LcPZ0O2XDYfa5OedFipN4Riw7iibTBCvUR6fVWnYx",
+                    'cache-control': "no-cache",
 					}
 
-		make_connection = httplib.HTTPSConnection("api.yelp.com")
+		make_connection = httplib2.HTTPSConnection("api.yelp.com")
 		res_id= food_place_name
-		make_connection.request("GET", "https://api.yelp.com/v3/businesses/"+res_id+"/reviews", headers=headers)
+		make_connection.request("GET", "GET https://api.yelp.com/v3/businesses/search", headers=headers)
 		result = make_connection.getresponse()
 		json_data = result.read()
 		json_data = json.loads(json_data.decode("utf-8"))
-		reviews=json_data['reviews']
+		reviews=json_data['businesses']
 		for review in reviews:
 			lis.append(review['user']['name'] + ':'+ ' '+ (review['text']))
 
-
-
-		#check if the restaurant name is in the zomato databse
-		search_results= Food_Place_ID_Zomato.objects.filter(name__startswith=input_box)
-		if(search_results):
-			both=True
-			#restaurant is in the zomato databse so setup the api call for getting reviews for this restaurant from zomato as well  (limit 5)
-			search_results1=search_results[0].restaraunt_id
-			cusine=search_results[0].cusine
-
-			entry=User_Detail(user_ID= str(request.user.username), user_name= cusine)
-			entry.save()
-
-			recipe="http://127.0.0.1:8000/database/recipes_results?query=" + str(cusine)
-			menu_url=search_results[0].menu_url
-			average_cost_for_two=search_results[0].average_cost_for_two
-
-			header = 	{
-						"User-agent": "curl/7.43.0",
-						'X-Zomato-API-Key': '4903b587901a5352f403a3a97da3543a'
-						}
-			url='https://developers.zomato.com/api/v2.1/reviews?res_id='
-			res_id=str(search_results1)
-			url=url+res_id
-			response = requests.get(url, headers=header)
-			reviews=response.json()
-
-			## parse Zomoato API response and collect all reviews in a list
-			reviews=reviews['user_reviews']
-			for review in reviews:
-				lis.append(review['review']['user']['name']+ ': '+ review['review']['review_text'])
 
 	#if user did not type in the exact name of the restaurant then check if the user entered a city name and if so return all restaurants with the user input city name
 	elif(Food_Place_ID_Yelp.objects.filter(city=input_box)):
@@ -145,20 +117,6 @@ def restaurants_results(request):
 	context={'search_results':lis, 'name':name, 'image':image, 'category':category, 'rating':rating, 'phone_num':phone_num, 'address':address, 'zip_code':zip_code, 'cusine':cusine, 'menu_url':menu_url, 'average_cost_for_two':average_cost_for_two, 'recipe':recipe }
 	return HttpResponse(template.render(context,request))
 
-#setup simple search function for recipes
-def find_recipes(request):
-	context= {}
-	template= get_template('find_recipes.html')
-	return HttpResponse(template.render(context, request))
-
-
-#find recipes either based on title or cusine based of the userinput given from the search recipe page
-def recipes_results(request):
-	template= get_template('recipes_results.html')
-	input_box= request.GET['query']
-	search_results= Recipe.objects.filter(title=input_box)
-	lis=[]
-
 	## a special search query which is activated if the user clicks on the reccomendation button on the homepage
 	if(input_box == 'user'):
 		##check user search history
@@ -179,21 +137,6 @@ def recipes_results(request):
 	elif(search_results):
 		lis.append("General Information ")
 		lis.append(' Cuisine: '+ search_results[0].cusine +' recipe title: '+ search_results[0].title + ' recipe ready time: ' + str(search_results[0].readyInMinutes) )
-
-	elif(True):
-		check_cusine=input_box[1:5]
-		search_results=Recipe.objects.filter(cusine__icontains=check_cusine)
-		for search_result in search_results:
-			lis.append('recipe id: '+ str(search_result.recipe_id) + ' Cuisine: '+ search_result.cusine +' recipe title: '+ search_result.title + ' recipe ready time: ' + str(search_result.readyInMinutes) )
-
-
-	elif(Recipe.objects.filter(cusine=input_box)):
-		lis=[]
-		lis.append("General Information ")
-		search_results=Recipe.objects.filter(cusine=input_box)
-		for search_result in search_results:
-			lis.append('recipe id: '+ str(search_result.recipe_id) + ' Cuisine: '+ search_result.cusine +' recipe title: '+ search_result.title + ' recipe ready time: ' + str(search_result.readyInMinutes) )
-
 
 	context= { 'search_results':lis}
 	return HttpResponse(template.render(context,request))
